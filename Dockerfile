3d9cfed2f127fbd812539cd89409c229dfc818ed
Jason Galea <nunya@business.meme>
2024-12-30 13:34:09 +1100

    feat: Install docker from repos, to fix OCI error

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,29 @@
 ### https://www.jenkins.io/doc/book/installing/docker/
+### https://docs.docker.com/engine/install/debian/
 
 FROM jenkins/jenkins:2.479.2-jdk21
 USER root
 
-RUN apt-get update && apt-get install -y lsb-release
-RUN curl -fsSLo /usr/share/keyrings/docker-archive-keyring.asc \
-    https://download.docker.com/linux/debian/gpg
-RUN echo "deb [arch=$(dpkg --print-architecture) \
-    signed-by=/usr/share/keyrings/docker-archive-keyring.asc] \
-    https://download.docker.com/linux/debian \
-    $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
-RUN apt-get update && apt-get install -y docker-ce-cli
+# RUN apt-get update && apt-get install -y lsb-release
+# RUN curl -fsSLo /usr/share/keyrings/docker-archive-keyring.asc \
+#     https://download.docker.com/linux/debian/gpg
+# RUN echo "deb [arch=$(dpkg --print-architecture) \
+#     signed-by=/usr/share/keyrings/docker-archive-keyring.asc] \
+#     https://download.docker.com/linux/debian \
+#     $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+# RUN apt-get update && apt-get install -y docker-ce-cli
+RUN apt update
+RUN apt install ca-certificates curl
+RUN install -m 0755 -d /etc/apt/keyrings
+RUN curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+RUN chmod a+r /etc/apt/keyrings/docker.asc
+RUN echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+RUN apt-get update
+RUN apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 
 USER jenkins
 RUN jenkins-plugin-cli --plugins "blueocean docker-workflow configuration-as-code"
--- bootstrap_jenkins.sh
+++ bootstrap_jenkins.sh
@@ -1,25 +1,38 @@
 #!/usr/bin/bash
 
 
+echo -e "\n==> [LXC] Set locales"
+sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+
+### https://docs.docker.com/engine/install/debian/
 echo -e "\n==> [LXC] Install docker"
-apt update ### This just happened a second ago
-apt upgrade -y
-# apt install -y git curl docker docker.io
-apt install -y curl docker docker.io
+# apt update ### This just happened a second ago
+# apt upgrade -y
+# apt install -y curl docker docker.io
+apt update
+# apt install -y curl
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
-COMPOSE_URL="https://github.com/docker/compose/releases/download/v2.32.0/docker-compose-linux-x86_64"
-COMPOSE_PATH="/root/.docker/cli-plugins/docker-compose"
+# COMPOSE_URL="https://github.com/docker/compose/releases/download/v2.32.0/docker-compose-linux-x86_64"
+# COMPOSE_PATH="/root/.docker/cli-plugins/docker-compose"
 
-if [[ ! -f $COMPOSE_PATH ]]; then
-    echo -e "\n==> [LXC] Install docker compose"
-    mkdir -p /root/.docker/cli-plugins
-    curl -SL $COMPOSE_URL -o $COMPOSE_PATH
-    chmod +x $COMPOSE_PATH
-    docker compose version
-else
-    echo -e "\n==> [LXC] Docker compose is already installed, continuing..."
-fi
+# if [[ ! -f $COMPOSE_PATH ]]; then
+#     echo -e "\n==> [LXC] Install docker compose"
+#     mkdir -p /root/.docker/cli-plugins
+#     curl -SL $COMPOSE_URL -o $COMPOSE_PATH
+#     chmod +x $COMPOSE_PATH
+#     docker compose version
+# else
+#     echo -e "\n==> [LXC] Docker compose is already installed, continuing..."
+# fi
 
 
 
