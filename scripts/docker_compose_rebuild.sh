05c057b2854363ace1a0f28a3e63c08c55cc10a2
Jason Galea <nunya@business.meme>
2024-12-30 17:13:13 +1100

    feat: docker_compose_rebuild.sh

--- scripts/docker_compose_rebuild.sh
+++ scripts/docker_compose_rebuild.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/bash
 
 
+echo -e "\n==> Compose down"
 docker compose -f compose/docker-compose.yml down
 
+
+echo -e "\n==> Delete data vol"
 docker volume rm compose_jenkins-data
 
+
+echo -e "\n==> Build new Jenkins image"
 docker compose -f compose/docker-compose.yml build
 
+
+echo -e "\n==> Compose up"
 docker compose -f compose/docker-compose.yml up -d
 
