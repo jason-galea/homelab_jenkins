2f4ad77cc4915b72081745af1cf379248bba7772
Jason Galea <nunya@business.meme>
2025-01-01 23:35:37 +1100

    feat: SSH agent

--- scripts/bootstrap_jenkins.sh
+++ scripts/bootstrap_jenkins.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/bash
 
+HOME="/root/homelab_jenkins"
+
+echo -e "\n==> [LXC] DEBUG"
+echo "id = $(id)"
+echo "pwd = $(pwd)"
+echo "HOME = $HOME"
+
+
+###################################################################
+### Prep OS
 echo -e "\n==> [LXC] Set locales"
 sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
@@ -18,6 +28,32 @@ echo \
 apt update
 apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+
+###################################################################
+### Prepare files for launch
+echo -e "\n==> [LXC] Generate SSH key to connect agents"
+KEYS_DIR="$HOME/jenkins_agent_keys"
+if [ ! -f "$KEYS_DIR/id_rsa" ]; then
+    mkdir $KEYS_DIR
+    ssh-keygen -t rsa -b 4096 -f "$KEYS_DIR/id_rsa" -N ""
+    chmod 600 "$KEYS_DIR/id_rsa"
+    chmod 644 "$KEYS_DIR/id_rsa.pub"
+else
+    echo -e "\n==> [LXC] SSH keys already exist. Skipping key generation."
+fi
+
+echo -e "\n==> [LXC] Template Jenkins config file"
+JENKINS_CONFIG=$HOME/config_as_code/jenkins.yml
+cp $HOME/templates/jenkins.template.yml $JENKINS_CONFIG
+
+sed -i "s|__JENKINS_IP__|$(hostname -i)|g" $JENKINS_CONFIG
+PRIV_KEY=$(grep -v 'OPENSSH PRIVATE KEY' $HOME/jenkins_agent_keys/id_rsa)
+sed -i "s|__SSH_CRED_PRIVATE_KEY__|$PRIV_KEY|g" $JENKINS_CONFIG
+
+
+
+###################################################################
+### Launch
 echo -e "\n==> [LXC] Docker compose pull"
 docker compose pull
 
@@ -32,4 +68,3 @@ echo -e "\n==> [LXC] Success! Your new Jenkins instance should be available at $
 
 echo -e "\n==> [LXC] Show initial admin password"
 docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword
-
